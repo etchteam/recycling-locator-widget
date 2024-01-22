9adb44e2afde7af3730dbb4271033643ef4afe05
Dan Webb <dan@etch.co>
2024-01-22 17:01:30 +0000

    feat: start setting up css variables

--- src/components/App/App.ts
+++ src/components/App/App.ts
@@ -2,23 +2,24 @@ import { LitElement, css } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { html } from 'lit/static-html.js';
 
+import { typography } from './styles/typography';
+import { variables } from './styles/variables';
+
 /**
  * Provides global styles for the application
  */
 @customElement('locator-app')
 export class App extends LitElement {
-  static styles = css`
-    :host {
-      --locator-outer-border-color: var(
-        --recycling-locator-border-color,
-        #bdbdbd
-      );
-
-      display: block;
-      font-family: Arial, Helvetica, sans-serif;
-      border: 1px solid var(--locator-outer-border-color);
-    }
-  `;
+  static styles = [
+    variables,
+    typography,
+    css`
+      :host {
+        display: block;
+        border: 1px solid var(--locator-container-border-color);
+      }
+    `,
+  ];
 
   render() {
     return html`<slot></slot>`;
---
+++ src/components/App/styles/variables.ts
@@ -0,0 +1,28 @@
+import { css } from 'lit';
+
+/**
+ * Public variables are prefixed with `--recycling-locator-`
+ * Private variables are prefixed with `--locator-`
+ */
+export const variables = css`
+  :host {
+    /* Colors */
+    --locator-color-body: #333;
+    --locator-color-border: #bdbdbd;
+
+    /* Fonts */
+    --locator-font-family: var(
+      --recycling-locator-font-family,
+      Arial,
+      Helvetica,
+      sans-serif
+    );
+
+    /* Container */
+    --locator-min-height: 540px;
+    --locator-container-border-color: var(
+      --recycling-locator-container-border-color,
+      var(--locator-color-border)
+    );
+  }
+`;
