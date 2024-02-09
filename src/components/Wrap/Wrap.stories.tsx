f09759d660d2f22ee1035c1f934fe5d563b0b4b4
Dan Webb <dan@etch.co>
2024-02-09 15:06:56 +0000

    feat: consistent wrap

---
+++ src/components/Wrap/Wrap.stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, StoryObj } from '@storybook/preact';
+
+import './Wrap';
+
+const meta: Meta = {
+  title: 'Components/Wrap',
+};
+
+export default meta;
+
+export const Wrap: StoryObj = {
+  render: () => (
+    <locator-wrap>
+      <div style="border:1px solid black">
+        Wrapped content with fluidly scaling max width
+      </div>
+    </locator-wrap>
+  ),
+};
--- src/pages/Start.tsx
+++ src/pages/Start.tsx
@@ -4,6 +4,7 @@ import '../components/Layout/Layout';
 import '../components/Logo/Logo';
 import '../components/Tip/Tip';
 import '../components/LocationInput/LocationInput';
+import '../components/Wrap/Wrap';
 
 export default function StartPage() {
   const { t } = useTranslation();
@@ -11,7 +12,7 @@ export default function StartPage() {
   return (
     <locator-layout>
       <locator-logo slot="header"></locator-logo>
-      <diamond-wrap gutter="lg" slot="main">
+      <locator-wrap slot="main">
         <diamond-section>
           <h2>{t('start.title')}</h2>
           <form>
@@ -24,15 +25,17 @@ export default function StartPage() {
             </diamond-button>
           </form>
         </diamond-section>
-      </diamond-wrap>
+      </locator-wrap>
       <locator-tip slot="aside">
-        <p>Use this service to:</p>
-        <ul>
-          <li>see your nearest places to recycle</li>
-          <li>find out how to recycle a specific item</li>
-          <li>check what you can recycle at home</li>
-        </ul>
-        <img src="/images/recycling-technology.webp" alt="" />
+        <locator-wrap>
+          <p>Use this service to:</p>
+          <ul>
+            <li>see your nearest places to recycle</li>
+            <li>find out how to recycle a specific item</li>
+            <li>check what you can recycle at home</li>
+          </ul>
+          <img src="/images/recycling-technology.webp" alt="" />
+        </locator-wrap>
       </locator-tip>
     </locator-layout>
   );
