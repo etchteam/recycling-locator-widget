59df17c79e15110b366fa8c6ea6d672678b6e16e
Dan Webb <dan@etch.co>
2024-02-19 11:24:21 +0000

    feat: get start path tests

--- src/lib/AppState.tsx
+++ src/lib/AppState.tsx
@@ -3,23 +3,13 @@ import { useContext } from 'preact/hooks';
 
 import { RecyclingLocatorAttributes } from '..';
 
+import getStartPath from './getStartPath';
+
 interface AppStateContext extends RecyclingLocatorAttributes {
   startPath: string;
 }
 
 export const AppState = createContext<AppStateContext>(null);
-
-function getStartPath(attributes: RecyclingLocatorAttributes): string {
-  const { postcode } = attributes;
-
-  if (attributes.path) {
-    return attributes.path;
-  } else if (attributes.postcode) {
-    return `/${postcode}`;
-  }
-
-  return '/';
-}
 
 export function createAppState(
   attributes: RecyclingLocatorAttributes,
