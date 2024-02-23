5618b831841a6259f53348920785a17c89da2f1a
Dan Webb <dan@etch.co>
2024-02-23 09:45:37 +0000

    feat: get locations for material query

--- src/pages/[postcode]/material/material.page.tsx
+++ src/pages/[postcode]/material/material.page.tsx
@@ -1,14 +1,19 @@
 import { useTranslation } from 'react-i18next';
 import { useLoaderData } from 'react-router-dom';
+import '@etchteam/diamond-ui/canvas/Card/Card';
 
+import '@/components/composition/Wrap/Wrap';
 import { MaterialLoaderResponse } from './material.loader';
 
 export default function MaterialPage() {
   const { t } = useTranslation();
-  const { recycleAtHome } = useLoaderData() as MaterialLoaderResponse;
-  const recyclable = recycleAtHome.schemes.some(
+  const { recycleAtHome, locations } =
+    useLoaderData() as MaterialLoaderResponse;
+  const recyclableAtHome = recycleAtHome.schemes.some(
     (scheme) => scheme.containers.length > 0,
   );
+  const recyclableNearby = locations.length > 0;
+  const recyclable = recyclableAtHome || recyclableNearby;
 
   return (
     <locator-hero variant={recyclable ? 'positive' : 'negative'}>
