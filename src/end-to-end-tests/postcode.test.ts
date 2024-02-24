4913c49b3a7748a95d668dd0012f64ded098b3d4
Dan Webb <dan@etch.co>
2024-02-24 10:12:33 +0000

    feat: debug label visibility

--- src/end-to-end-tests/postcode.test.ts
+++ src/end-to-end-tests/postcode.test.ts
@@ -19,21 +19,21 @@ describeEndToEndTest('Postcode location search', () => {
     });
     console.log(2);
     const label = page.getByText(i18n.t('start.label'));
-    const input = page.getByLabel(i18n.t('start.label')).first();
+    // const input = page.getByLabel(i18n.t('start.label')).first();
     console.log(3);
-    const notInUk = page.getByText(i18n.t('notFound.title.notInTheUK')).first();
+    // const notInUk = page.getByText(i18n.t('notFound.title.notInTheUK')).first();
     console.log(4, i18n.t('start.label'));
     await expect(label).toBeVisible({ timeout: 30000 });
     console.log(5);
-    await expect(input).toBeVisible({ timeout: 30000 });
-    console.log(5);
-    await expect(notInUk).not.toBeVisible();
-    console.log(6);
-    await input.fill('Guernsey');
-    console.log(7);
-    await input.press('Enter');
-    console.log(8);
-    await expect(notInUk).toBeVisible();
+    // await expect(input).toBeVisible({ timeout: 30000 });
+    // console.log(5);
+    // await expect(notInUk).not.toBeVisible();
+    // console.log(6);
+    // await input.fill('Guernsey');
+    // console.log(7);
+    // await input.press('Enter');
+    // console.log(8);
+    // await expect(notInUk).toBeVisible();
   }, 30000);
 
   test('Location found page shows with the valid postcode + city entered', async ({
