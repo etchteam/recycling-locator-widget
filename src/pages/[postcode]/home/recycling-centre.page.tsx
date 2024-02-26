9e11806c005fe1ba8f4831a55fa0d9d880ca5029
Dan Webb <dan@etch.co>
2024-02-26 17:03:19 +0000

    feat: recycle centre alert

--- src/pages/[postcode]/home/recycling-centre.page.tsx
+++ src/pages/[postcode]/home/recycling-centre.page.tsx
@@ -15,42 +15,52 @@ export default function HomeRecyclingCentrePage() {
 
   return (
     <>
-      <h3>{t(`${tContext}.title`)}</h3>
-      <p>{t(`${tContext}.content`, { count: 1 })}</p>
-      <diamond-card radius>
-        <locator-icon-text>
-          <locator-icon icon="search"></locator-icon>
-          <p>{t(`${tContext}.info`)}</p>
-        </locator-icon-text>
-      </diamond-card>
-
-      <h3>{t(`${tContext}.nearbyPlaces.title`)}</h3>
-      <diamond-card border radius>
-        <locator-icon-text className="diamond-spacing-bottom-xs">
-          <locator-icon-circle variant="negative">
-            <locator-icon icon="place"></locator-icon>
-          </locator-icon-circle>
-          <h3>{t(`${tContext}.nearbyPlaces.content`, { count: 1 })}</h3>
-        </locator-icon-text>
-        <diamond-button width="full-width">
-          <diamond-grid>
-            <diamond-grid-item small-mobile="6">
-              <diamond-button width="full-width">
-                <Link to={`/${postcode}/places-list?${location.search}`}>
-                  {t('actions.listPlaces')}
-                </Link>
-              </diamond-button>
+      <section className="diamond-spacing-bottom-lg">
+        <h3>{t(`${tContext}.title`)}</h3>
+        <p>{t(`${tContext}.content`, { count: 1 })}</p>
+        <diamond-card className="theme-info" padding="sm" radius>
+          <diamond-grid gap="sm">
+            <diamond-grid-item>
+              <locator-icon icon="info"></locator-icon>
             </diamond-grid-item>
-            <diamond-grid-item small-mobile="6">
-              <diamond-button width="full-width">
-                <Link to={`/${postcode}/places-map?${location.search}`}>
-                  {t('actions.showMap')}
-                </Link>
-              </diamond-button>
+            <diamond-grid-item grow shrink>
+              <p className="diamond-text-size-sm">{t(`${tContext}.info`)}</p>
             </diamond-grid-item>
           </diamond-grid>
-        </diamond-button>
-      </diamond-card>
+        </diamond-card>
+      </section>
+
+      <section className="diamond-spacing-bottom-lg">
+        <h3 className="diamond-spacing-bottom-md">
+          {t(`${tContext}.nearbyPlaces.title`)}
+        </h3>
+        <diamond-card border radius>
+          <locator-icon-text className="diamond-spacing-bottom-md">
+            <locator-icon-circle variant="positive">
+              <locator-icon icon="place"></locator-icon>
+            </locator-icon-circle>
+            <h3>{t(`${tContext}.nearbyPlaces.content`, { count: 1 })}</h3>
+          </locator-icon-text>
+          <diamond-button width="full-width">
+            <diamond-grid>
+              <diamond-grid-item small-mobile="6">
+                <diamond-button width="full-width">
+                  <Link to={`/${postcode}/places-list?${location.search}`}>
+                    {t('actions.listPlaces')}
+                  </Link>
+                </diamond-button>
+              </diamond-grid-item>
+              <diamond-grid-item small-mobile="6">
+                <diamond-button width="full-width">
+                  <Link to={`/${postcode}/places-map?${location.search}`}>
+                    {t('actions.showMap')}
+                  </Link>
+                </diamond-button>
+              </diamond-grid-item>
+            </diamond-grid>
+          </diamond-button>
+        </diamond-card>
+      </section>
     </>
   );
 }
