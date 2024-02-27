541d717097071ec968b0a6ada91885bd6cc2c80a
Dan Webb <dan@etch.co>
2024-02-27 09:30:20 +0000

    feat: home recycling contact

--- src/pages/[postcode]/home/contact.page.tsx
+++ src/pages/[postcode]/home/contact.page.tsx
@@ -1,3 +1,53 @@
-export default function HomeRecyclingContactPage() {
-  return <p>Home recycling contact</p>;
+import { useTranslation } from 'react-i18next';
+import '@etchteam/diamond-ui/canvas/Card/Card';
+
+import '@/components/composition/Container/Container';
+import '@/components/content/ContainerSvg/ContainerSvg';
+import '@/components/composition/BorderedList/BorderedList';
+
+import { useHomeRecyclingLoaderData } from './home.loader';
+
+export default function HomeRecyclingPage() {
+  const { t } = useTranslation();
+  const { localAuthority } = useHomeRecyclingLoaderData();
+  const tContext = 'homeRecycling.contact';
+
+  return (
+    <>
+      <h3>{t(`${tContext}.title`)}</h3>
+      <locator-bordered-list size="sm">
+        <h4>{localAuthority.name}</h4>
+        <dl>
+          <div>
+            <dt>{t(`${tContext}.website`)}</dt>
+            <dd>
+              <a
+                href={localAuthority.coreInformation.recyclingUri}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {localAuthority.coreInformation.recyclingUri}
+              </a>
+            </dd>
+          </div>
+          <div>
+            <dt>{t(`${tContext}.phone`)}</dt>
+            <dd>{localAuthority.coreInformation.enquiryNumber}</dd>
+          </div>
+          <div>
+            <dt>{t(`${tContext}.notes.title`)}</dt>
+            <dd>
+              <p>{t(`${tContext}.notes.paragraphOne`)}</p>
+              <ul>
+                {(t(`${tContext}.notes.list`) as string[]).map((item) => (
+                  <li key={item}>{item}</li>
+                ))}
+              </ul>
+              <p>{t(`${tContext}.notes.paragraphTwo`)}</p>
+            </dd>
+          </div>
+        </dl>
+      </locator-bordered-list>
+    </>
+  );
 }
