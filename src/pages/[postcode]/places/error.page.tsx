dafcc41714930b70b2bd3e1dcd37a44ffd1bbce2
Dan Webb <dan@etch.co>
2024-03-03 16:16:57 +0000

    fix: error page layout

--- src/pages/[postcode]/places/error.page.tsx
+++ src/pages/[postcode]/places/error.page.tsx
@@ -17,27 +17,16 @@ export default function PlacesErrorPage() {
 
   return (
     <PlacesLayout>
-      <h2>{t('error.title')}</h2>
-      <p>{t('places.error.message')}</p>
-      <p className="diamond-spacing-bottom-md">{t('error.message')}</p>
-      <diamond-button width="full-width" variant="primary">
-        <Link to={`/${postcode}/places`}>{t('actions.tryAgain')}</Link>
-      </diamond-button>
-      <locator-tip slot="layout-aside" text-align="center">
-        {/* TODO(WRAP-232): swap this out for the proper tip once we have content */}
+      <diamond-section padding="lg">
         <locator-wrap>
-          <img src="/images/recycling-technology.webp" alt="" />
-          <p className="diamond-text-weight-bold">Hints and tips</p>
-          <h2>How to check if your electricals can be recycled</h2>
-          <p>
-            Any items that have a plug, use batteries, need charging or have a
-            picture of a crossed out wheelie bin on, are known as Waste
-            Electrical and Electronic Equipment (WEEE). These items should not
-            be sent to landfill and should be recycled at Recycling Centres,
-            electrical item bring banks or via electrical retailers
-          </p>
+          <h2>{t('error.title')}</h2>
+          <p>{t('places.error.message')}</p>
+          <p className="diamond-spacing-bottom-md">{t('error.message')}</p>
+          <diamond-button width="full-width" variant="primary">
+            <Link to={`/${postcode}/places`}>{t('actions.tryAgain')}</Link>
+          </diamond-button>
         </locator-wrap>
-      </locator-tip>
+      </diamond-section>
     </PlacesLayout>
   );
 }
