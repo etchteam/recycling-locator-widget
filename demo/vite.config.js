527df2b9c71dde66a928656cbab12c7629b7c776
Dan Webb <dan@etch.co>
2024-03-12 14:29:16 +0000

    docs: add non-index entrypoints to demo

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -1,3 +1,5 @@
+import { resolve } from 'node:path';
+
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
@@ -8,6 +10,15 @@ export default defineConfig(({ mode }) => {
       'process.env': env,
     },
     base: '/recycling-locator',
+    build: {
+      rollupOptions: {
+        input: {
+          index: resolve(__dirname, 'index.html'),
+          welsh: resolve(__dirname, 'welsh.html'),
+          branded: resolve(__dirname, 'branded.html'),
+        },
+      },
+    },
   };
 
   return config;
