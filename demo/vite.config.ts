fd8b994a20e2dab5580962ecb0b2a11d8673c9cc
Dan Webb <dan@etch.co>
2024-03-12 06:34:50 +0000

    docs: blind demo build fix

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig, UserConfig } from 'vite';
+import { defineConfig, loadEnv, UserConfig } from 'vite';
 
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
   const config: UserConfig = {
+    define: {
+      'process.env': env,
+    },
     base: '/recycling-locator',
     server: {
       port: 3040,
