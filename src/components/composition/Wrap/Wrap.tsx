0f65e15bbcfc7483d83dc320280d90e7679fe4cc
Dan Webb <dan@etch.co>
2024-03-20 14:53:48 +0000

    feat: fluid places page gutter

--- src/components/composition/Wrap/Wrap.tsx
+++ src/components/composition/Wrap/Wrap.tsx
@@ -1,9 +1,14 @@
 import { CustomElement } from '@/types/customElement';
 
+export interface WrapAttributes {
+  gutter?: 'fluid';
+  'max-width'?: 'none';
+}
+
 declare module 'react' {
   namespace JSX {
     interface IntrinsicElements {
-      'locator-wrap': CustomElement;
+      'locator-wrap': CustomElement<WrapAttributes>;
     }
   }
 }
