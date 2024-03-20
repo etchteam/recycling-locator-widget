1d981f5d7f906a3e7fc1275f0b814916f2e8838a
Dan Webb <dan@etch.co>
2024-03-20 09:47:22 +0000

    fix: safari map fab

--- src/components/control/Fab/Fab.tsx
+++ src/components/control/Fab/Fab.tsx
@@ -5,6 +5,10 @@ export interface FabAttributes {
    * Where to place the button, if no position is given it will be placed at the bottom
    */
   position?: 'top';
+  /**
+   * If the button should scroll with the page
+   */
+  sticky?: boolean;
 }
 
 declare module 'react' {
