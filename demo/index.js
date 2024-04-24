c4c3417afccb8efe976ef290227f4784c9c3efaa
Dan Webb <dan@etch.co>
2024-04-24 11:01:30 +0100

    feat: temproarily disable marker feedback [skip ci]

--- demo/index.js
+++ demo/index.js
@@ -1,13 +1,1 @@
-import '@etchteam/recycling-locator/dist/index.js';
-
-const MARKER_KEY = import.meta.env.VITE_MARKER_KEY;
-
-if (MARKER_KEY) {
-  window.markerConfig = {
-    project: MARKER_KEY,
-    source: 'snippet',
-  };
-
-  /* eslint-disable-next-line */
-  !function(e,r,a){if(!e.__Marker){e.__Marker={};var t=[],n={__cs:t};["show","hide","isVisible","capture","cancelCapture","unload","reload","isExtensionInstalled","setReporter","setCustomData","on","off"].forEach(function(e){n[e]=function(){var r=Array.prototype.slice.call(arguments);r.unshift(e),t.push(r)}}),e.Marker=n;var s=r.createElement("script");s.async=1,s.src="https://edge.marker.io/latest/shim.js";var i=r.getElementsByTagName("script")[0];i.parentNode.insertBefore(s,i)}}(window,document);
-}
+import '@etchteam/recycling-locator';
