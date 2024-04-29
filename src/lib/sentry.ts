de88c7f62c6edc0da5e24f2c02ef05ef5fb18505
Dan Webb <dan@etch.co>
2024-04-29 14:07:17 +0100

    fix: enable sentry integrations

--- src/lib/sentry.ts
+++ src/lib/sentry.ts
@@ -8,5 +8,15 @@ Sentry.init({
   enabled: Boolean(DSN),
   dsn: DSN,
   release: `recycling-locator@${config.packageVersion}`,
+  // Prevent auto capturing of global errors from host sites
   defaultIntegrations: false,
+  // Enable integrations that can be used without capturing host site errors
+  // https://docs.sentry.io/platforms/javascript/configuration/integrations/#modifying-default-integrations
+  integrations: [
+    Sentry.httpContextIntegration(),
+    Sentry.linkedErrorsIntegration(),
+    Sentry.dedupeIntegration(),
+    Sentry.breadcrumbsIntegration(),
+    Sentry.functionToStringIntegration(),
+  ],
 });
